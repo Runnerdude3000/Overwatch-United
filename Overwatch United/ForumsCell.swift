69835eb6e43db89abe19efbdaa5f9e964175b6f0
John Nicholson <johnfnicholson123@gmail.com>
2017-03-16 19:20:22 -0400

    Touched up forums UI and added IBOutlets

---
+++ Overwatch United/ForumsCell.swift
@@ -0,0 +1,25 @@
+//
+//  ForumsCell.swift
+//  Overwatch United
+//
+//  Created by Johnny Nicholson on 3/14/17.
+//  Copyright © 2017 Johnny Nicholson. All rights reserved.
+//
+
+import UIKit
+
+class ForumsCell: UITableViewCell
+{
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+
+    }
+
+}
--- Overwatch United/ForumsScreenVC.swift
+++ Overwatch United/ForumsScreenVC.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 
-class ForumsScreenVC: UIViewController
+class ForumsScreenVC: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
 
 
     }
@@ -22,6 +26,23 @@ class ForumsScreenVC: UIViewController
     {
         dismiss(animated: true, completion: nil)
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        return tableView.dequeueReusableCell(withIdentifier: "ForumsCell") as! ForumsCell
+    }
+    
+    
     
 
 }
